5c8c97a6eda6095a6f4249e012e1d232aed65f93
Juniper <basil.squared1@gmail.com>
2026-07-06 07:44:29 -0500

    i plucked an actual value out  of it now, and now i properly closed everything out. no memory leaks for me!!

--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 #include <lua.h>
 #include <lualib.h>
 #include <lauxlib.h>
-
+#include <string.h>
 int main(void) {
     char buf[256];
     int error;
@@ -12,6 +12,13 @@ int main(void) {
         printf("error: %s\n", lua_tostring(L, -1));
         return 1;
     }
+    // Lua's stack goes -1,-2,-3 for searching from the top and 1,2,3 from the bottom. important things to remember!!! 
+    lua_getfield(L,-1,"abilities");
+    lua_getfield(L,-1,"kind");
+    const char* kind = lua_tostring(L,-1);
+    char *kind_copy = strdup(kind);
+    free(kind_copy); 
+    lua_close(L);
 
 
 
